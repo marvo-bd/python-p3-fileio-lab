e6bf97bd8f3fbeefa32bdda6c4b6287aaa72d5ab
Marvin Kabira <marvobd@gmail.com>
2024-07-23 16:37:50 +0300

    lab completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Writes content to a .txt file."""
+    with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to a .txt file."""
+    with open(f"{file_name}.txt", 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Reads content from a .txt file."""
+    with open(f"{file_name}.txt", 'r') as file:
+        return file.read()
